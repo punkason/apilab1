0a4ac14724b191d785ee125cb8cfb903412f7da4
punkason <punkason@gmail.com>
2022-02-15 10:39:39 +0000

    add CoffeeController and test case

---
+++ src/main/java/com/apilab/apilab1/controller/CoffeeController.java
@@ -0,0 +1,17 @@
+package com.apilab.apilab1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoffeeController {
+
+    @GetMapping(value = "/coffeelover")
+    public String home() {
+        return "I like coffee!";
+    }
+
+}
