0bce332b50d20b35543a6094a18b72b48f6a0cb6
punkason <punkason@gmail.com>
2022-02-15 10:51:58 +0000

    add Coffee.java

--- src/main/java/com/apilab/apilab1/controller/CoffeeController.java
+++ src/main/java/com/apilab/apilab1/controller/CoffeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CoffeeController {
 
     @GetMapping(value = "/coffeelover")
-    public String home() {
+    public String coffee() {
         return "I like coffee!";
     }
 
