905b88b947c2dd63cda5796a865fb748737578e0
punkason <punkason@gmail.com>
2022-02-15 10:30:45 +0000

    add HomeController

---
+++ src/main/java/com/apilab/apilab1/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.apilab.apilab1.controller;
+
+//package definition omitted for brevity
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping(value = "/")
+    public String home() {
+        return "Welcome to the Drinks API!";
+    }
+
+}
